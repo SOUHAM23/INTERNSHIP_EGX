a1419279d353cb22aaf7bc626df0ab1f2c1aad66
Souham Dutta <104433940+SOUHAM23@users.noreply.github.com>
2025-05-13 21:06:26 +0530

    Delete Day4 directory

--- Day4/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-print(df.columns)
-# print(df[df['season'] == 'rainy'])
-
-# print("UNIQUE VALUES based on the season ")
-# unique_crops_by_season = df.groupby('season')['label'].unique()
-# print(unique_crops_by_season)
